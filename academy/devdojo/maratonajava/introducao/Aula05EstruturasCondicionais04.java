94eeb1231df7b0c85efa56c077f1abb40241c800
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-08-14 20:04:15 -0300

    condicionais

--- academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -10,14 +10,15 @@ public class Aula05EstruturasCondicionais04 {
         double primeiraFaixa = 9.7 / 100;
         double segundaFaixa = 37.35 / 100;
         double terceiraFaixa = 49.50 / 100;
+        double valorImposto;
 
         if(salarioAnual <= 34712 ){
-            salarioAnual = salarioAnual * primeiraFaixa;
+            valorImposto = salarioAnual * primeiraFaixa;
         } else if (salarioAnual >= 34713 && salarioAnual <= 68507) {
-            salarioAnual = salarioAnual * segundaFaixa;
+            valorImposto = salarioAnual * segundaFaixa;
         }else{
-            salarioAnual = salarioAnual * terceiraFaixa;
+            valorImposto = salarioAnual * terceiraFaixa;
         }
-        System.out.println(salarioAnual);
+        System.out.println(valorImposto);
     }
 }
