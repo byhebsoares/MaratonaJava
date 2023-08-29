4225e3495a73369eef22593067892eae363ff042
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-08-29 07:20:50 -0300

    Array
    Nesta aula vi as iniciações padroes dos tipos primitivos e references de um array não inicializado

---
+++ academy/devdojo/maratonajava/introducao/Aula06EstruturaDeRepetiao02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturaDeRepetiao02 {
+    public static void main(String[] args) {
+        //Imprima todos os numeros pares de 0 até 1000000
+
+        for (int i = 0; i <= 1000000; i++){
+            if ((i % 2) == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //
+        int[] idades = new int[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
