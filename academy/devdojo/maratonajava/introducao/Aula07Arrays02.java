dc96e08e06f189fa10ad21b2244865f2b351f0a5
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-08-29 20:52:22 -0300

    arrays

--- academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -2,10 +2,13 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        //
-        int[] idades = new int[3];
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurusaki";
+        nomes[2] = "Luffy";
+        for (int i = 0; i < 3; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
