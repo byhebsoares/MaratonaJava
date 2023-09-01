199815b806ce167253de13364ab657d5b8998f3b
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-08-31 21:15:26 -0300

    POO Exercicios

---
+++ academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+//Exercicio criar 1 classe carro e criar 2 classe
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Carro carro1 = new Carro();
+
+        carro.nome = "BMW";
+        carro.ano = 2022;
+        carro.modelo = "Hach";
+
+        carro1.nome = "Mercedez";
+        carro1.ano = 2022;
+        carro1.modelo = "Sport";
+
+        System.out.println("Nome: " + carro.nome + " Ano: " + carro.ano + " Modelo: " + carro.modelo);
+        System.out.println("Nome: " + carro1.nome + " Ano: " + carro1.ano + " Modelo: " + carro1.modelo);
+    }
+}
