ac15f1bcf818d59d9c5cc8baadf100cd940f0d85
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-09-02 19:34:48 -0300

    exercicio

---
+++ academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double salario1;
+    public double salario2;
+    public double salario3;
+
+    public void imprime(){
+        System.out.println("Nome funcionario: " + this.nome);
+        System.out.println("Idade funcionario: " + this.idade + " Anos");
+        System.out.println("Salário 1: " + this.salario1);
+        System.out.println("Salário 2: " + this.salario2);
+        System.out.println("Salário 3: " + this.salario3);
+        mediaSalario();
+    }
+    public void mediaSalario(){
+        System.out.println("media salarial: " + (salario1+salario2+salario3)/3);
+    }
+}
---
+++ academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Hebert Bezerra Soares";
+        funcionario.idade = 26;
+        funcionario.salario1 = 1600;
+        funcionario.salario2 = 1800;
+        funcionario.salario3 = 5000;
+
+        funcionario.imprime();
+
+    }
+}
