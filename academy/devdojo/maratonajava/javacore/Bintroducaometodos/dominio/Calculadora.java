2cf55799d3a47bf2c618711cfc502ebf1281120b
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-09-01 23:14:28 -0300

    varargs

--- academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -31,11 +31,25 @@ public class Calculadora {
             System.out.println(num1 / num2);
         }
     }
-    public void alteraDoisNumeros(int num1,int num2){
-        num1 = 99;
-        num2 = 33;
+    public void alteraDoisNumeros(int numero1,int numero2){
+        numero1 = 99;
+        numero2 = 33;
         System.out.println("Dentro do alteraDoisNumeros");
-        System.out.println("Num1 " + num1);
-        System.out.println("Num2 " + num2);
+        System.out.println("Num1 " + numero1);
+        System.out.println("Num2 " + numero2);
+    }
+    public void somaArray(int[] numeros){
+        int soma = 0;
+        for (int num: numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+    public void somaVarArgs(int...numeros){
+        int soma = 0;
+        for (int num: numeros){
+            soma += num;
+        }
+        System.out.println(soma);
     }
 }
