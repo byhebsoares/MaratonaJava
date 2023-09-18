b652f8ee429fede2dd8b1c3e82b46e4c2a664078
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-09-18 20:46:16 -0300

    Heranca

---
+++ academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("08452-140");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyota");
+        pessoa.setCpf("14003654800");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+    }
+}
