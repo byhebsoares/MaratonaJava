155237c3251d89e2e0969096266a7cae122bd84b
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-09-27 19:06:45 -0300

    runtime 02

--- academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
+++ academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -3,8 +3,16 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 public class RuntimeExceptionTest02 {
     public static void main(String[] args) {
         divisao(1,0);
+        System.out.println("Código finalizado");
+        /***
+         *
+         * @param a
+         * @param b não pode ser zero
+         * @return
+         * throws illegalArgumentException caso b seja zero
+         */
     }
-    private static int divisao(int a, int b){
+    private static int divisao(int a, int b) throws IllegalArgumentException {
         if (b == 0){
             throw new IllegalArgumentException("Argumento ilegal, não pode ser zero");
         }
