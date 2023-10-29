3cbb23e0eebb4349ad1df2747e323303bce3bf9b
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-10-29 09:05:30 -0300

    att

--- src/javacore/Vio/test/BufferedWriterTest01.java
+++ src/javacore/Vio/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file,true);
-             BufferedWriter br = new BufferedWriter(fw)){
-             br.write("O herb eh lindo melhor aluno do brasil");
-             br.newLine();
-             br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)){
+             bw.write("O herb eh lindo melhor aluno do brasil");
+             bw.newLine();
+             bw.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
