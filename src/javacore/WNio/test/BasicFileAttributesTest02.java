a9e1786a30f2b34c6f2fbd89daa803fa043594a7
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-11-01 20:03:25 -0300

    NIO pt 07 - BasicFileAttributes pt 02

---
+++ src/javacore/WNio/test/BasicFileAttributesTest02.java
@@ -0,0 +1,22 @@
+package src.javacore.WNio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+public class BasicFileAttributesTest02 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("pasta2/novo_path.txt");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        FileTime creationTime = basicFileAttributes.creationTime();
+        FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
+        FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
+
+        System.out.println("CreationTime " + creationTime);
+        System.out.println("lastModifiedTime " + lastModifiedTime);
+        System.out.println("lastAccessTime " + lastAccessTime);
+    }
+}
