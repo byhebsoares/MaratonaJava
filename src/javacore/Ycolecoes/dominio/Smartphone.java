0eb44a7a3d48c7e93c15016a59fd5eaa0b3941ad
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2023-11-13 20:32:05 -0300

    hashcode

--- src/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/javacore/Ycolecoes/dominio/Smartphone.java
@@ -3,6 +3,15 @@ package src.javacore.Ycolecoes.dominio;
 public class Smartphone {
     private String serialNumber;
     private String marca;
+    // se x.equals(y) == true, y.hashcode() == x.hashcode()
+    // y.hashcode() == x.hashcode() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashcode() != x.hashcode() x.equals(y) deverá ser false.
+
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
 
     public Smartphone(String serialNumber, String marca) {
         this.serialNumber = serialNumber;
